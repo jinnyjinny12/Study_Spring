0a31c62052332f03904863b2e16df48e8ea23941
jinny <eujin.mstory@gmail.com>
2024-06-23 22:10:58 +0900

    backup

--- Spring/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ Spring/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -1,18 +1,2 @@
-package hello.hello_spring.controller;
-
-
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class HelloController {
-
-    @GetMapping("Hello")
-    public String hello(Model model) {
-        model.addAttribute("data","hello!!!!");
-        return "hello";
-    }
-
+package hello.hello_spring.controller;public class HelloController {
 }
